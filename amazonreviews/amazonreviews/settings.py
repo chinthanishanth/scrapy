8ebcc9186400f3cb6d5fc8865a7aa766bf108e6b
chinthanishanth <chinthanishanth@gmail.com>
2019-05-11 18:17:52 +0000

    activated google user agent in settings and fixed indentation issue of parse method in amazon.py spider

--- amazonreviews/amazonreviews/settings.py
+++ amazonreviews/amazonreviews/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'amazonreviews.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'amazonreviews (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
