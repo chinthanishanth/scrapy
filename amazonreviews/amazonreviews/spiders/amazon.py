2eb300477e171b25af153f0635053177e69453d2
chintha nishanth <chinthanishanth@gmail.com>
2019-05-12 06:29:39 +0000

    finally divided each review as a chunck from amazon

--- amazonreviews/amazonreviews/spiders/amazon.py
+++ amazonreviews/amazonreviews/spiders/amazon.py
@@ -11,43 +11,22 @@ from amazonreviews.items import AmazonreviewsItem
 class AmazonSpider(scrapy.Spider):
     name = 'amazon'
     allowed_domains = ['web']
-    start_urls = ['https://www.amazon.in/Apple-iPhone-Silver-64GB-Storage/dp/B0711T2L8K/ref=sr_1_1?pf_rd_i=1389401031&pf_rd_m=A1K21FY43GMZF8&pf_rd_p=66601440-7bcb-4e94-aaa3-7f6a21c2ed6f&pf_rd_r=73QJ3CHSR9MFVEG73A6V&pf_rd_s=center-1&pf_rd_t=101&qid=1557424649&refinements=p_89%3AApple&s=electronics&sr=1-1']
-
+    start_urls = ['https://www.amazon.in/Apple-iPhone-X-64GB-Silver/product-reviews/B0711T2L8K/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews']
     def parse(self, response):
-
-        # text = response.xpath(
-        #     '//*[@data-hook="review-body"][1]//text() | div[@data-hook="review-collapsed"][1]//text()').extract()
-        # yield {'text': text}
-        # star_rating = response.xpath(
-        #     '//*[@data-hook="review-star-rating"][1]//text()').re('^[0-9]')
-        # yield {'star_rating': star_rating}
 
         item = AmazonreviewsItem()
 
-        all_reviews_div = response.xpath('//div[@data-hook="review"]')
-
-        # review_txt = all_reviews_div.xpath(
-        #     '//*[@data-hook="review-body"]//text() | div[@data-hook="review-collapsed"]//text()')
-
-        # rating = all_reviews_div.xpath(
-        #     '//*[@data-hook="review-star-rating"]//text()')
-
-        count = 0
+        all_reviews_div = response.xpath(
+            '//div[@data-hook="review"]')
 
         for review in range(0, len(all_reviews_div)):
 
-            print(len(all_reviews_div))
+            review_txt = all_reviews_div[review].xpath(
+                './/*[@data-hook="review-body"]//text()').extract()
 
-            data = all_reviews_div[review].xpath(
-                '//*[@data-hook="review-body"]//text() | div[@data-hook="review-collapsed"]//text()').extract()
+            rating = all_reviews_div[review].xpath(
+                './/*[@data-hook="review-star-rating"]//text()').re('^[0-9]')
 
-            print(data)
-
-            # item['review_txt'] = review_txt
-            # item['rating'] = rating
-            # yield {
-            #     "review_txt": ''.join(review.xpath('//text()').extract()),
-            #     "rating": ''.join(rating[count].xpath('//text()').re('^[0-9]'))
-
-            # }
-            # count = count+1
+            item['review_txt'] = review_txt
+            item['rating'] = rating
+            yield item
