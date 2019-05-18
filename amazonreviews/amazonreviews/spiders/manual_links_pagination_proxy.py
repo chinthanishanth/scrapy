e56b7bc657ac6087c9c55b4582201003a97fd862
chintha nishanth <chinthanishanth@gmail.com>
2019-05-18 15:50:26 +0000

    added roatating proxies in settings to prevent ban from websites

--- amazonreviews/amazonreviews/spiders/manual_links_pagination_proxy.py
+++ amazonreviews/amazonreviews/spiders/manual_links_pagination_proxy.py
@@ -3,9 +3,11 @@ import scrapy
 from amazonreviews.items import AmazonreviewsItem
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import MapCompose, Join
+from urllib.parse import urljoin
+from scrapy.http import Request, Response
+from scrapy.settings.default_settings import DOWNLOADER_MIDDLEWARES
 import datetime
 import socket
-
 # exporting package list from  conda venv to req.txt
 # conda list -e > req.txt
 # creating a venv in  conda and installing packages from req.txt
@@ -13,18 +15,16 @@ import socket
 
 
 class AmazonSpider(scrapy.Spider):
-    name = 'amazon_itemloader'
-    allowed_domains = ['web']
+    name = 'links_manual'
+    allowed_domains = ['https://www.amazon.in']
     start_urls = ['https://www.amazon.in/Apple-iPhone-X-64GB-Silver/product-reviews/B0711T2L8K/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews']
 
     def parse(self, response):
         '''
-        this functions scraps all the amazon reviews from the above link, using itemloaders to
-        simplify the code
+        This function extracts all the reviews and pagination links from the reviews of above amazon link
         '''
 
-        all_reviews_div = response.xpath(
-            '//div[@data-hook="review"]')
+        all_reviews_div = response.xpath('//div[@data-hook="review"]')
 
         for review in range(0, len(all_reviews_div)):
 
@@ -44,5 +44,15 @@ class AmazonSpider(scrapy.Spider):
             # using MapCompose for preprocessing items: converting datatime object to string
             i.add_value('date', datetime.date.today(),
                         MapCompose(lambda x: x.strftime('%Y/%m/%d')))
+            i.add_value('ipaddr', response.headers.get('X-Proxymesh-Ip'))
 
             yield i.load_item()
+
+    # identify the next button for looping review urls to extract more urls
+        next_link = response.xpath(
+            '//*[@data-hook="pagination-bar"]//li[@class ="a-last"]//a/@href').extract_first()
+
+        if next_link:
+            next_link = response.urljoin(next_link)
+            # next_link = urljoin('https://www.amazon.in', next_link)
+            yield scrapy.Request(url=next_link, callback=self.parse, dont_filter=True)
--- amazonreviews/runner.py
+++ amazonreviews/runner.py
@@ -1,13 +1,14 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
-from amazonreviews.spiders import amazon, amazon_itemloader, manual_links, manual_links_items
+from amazonreviews.spiders import amazon, amazon_itemloader, manual_links, manual_links_items, manual_links_pagination_proxy
 
 process = CrawlerProcess({
 })
 
 # process.crawl(amazon.AmazonSpider)
-# process.crawl(amazon_itemloader.AmazonSpider)
+process.crawl(amazon_itemloader.AmazonSpider)
 # process.crawl(manual_links.AmazonSpider)
-process.crawl(manual_links_items.AmazonSpider)
+# process.crawl(manual_links_items.AmazonSpider)
+# process.crawl(manual_links_pagination_proxy.AmazonSpider)
 process.start()  # the script will block here until the crawling is finished
